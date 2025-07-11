d848287c4bba2c8dc00227e2ee821154882c4032
hend-elsayed-github <ehend0713@gmail.com>
2025-07-11 03:15:47 +0300

    init

---
+++ src/app/owner/owner.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-owner',
+  imports: [],
+  templateUrl: './owner.component.html',
+  styleUrl: './owner.component.css'
+})
+export class OwnerComponent {
+
+}
